1908e5e4bc1ebaeb6767cc3aff8fac17b647d7c4
sourav das <dass7550@gmail.com>
2023-08-20 19:48:37 +0600

    Header ribon designed

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
-    
+    const[dropClass,setdropClass]=useState('dropdown-content-header')
+    const [user,setUser]=useState({});
+    useEffect(()=>{
+        fetch("user.json")
+        .then(res=>res.json())
+        .then(res=>setUser(res))
+    },[])
+
     const profileButton=()=>{
+        setdropClass('dropdown-content-header-displa') 
         console.log("/profile")
+    }
+    const profileButtonBlur=()=>{
+        setdropClass('dropdown-content-header')
     }
 
     return (
@@ -18,10 +29,21 @@ const Header = () => {
                 <CustomLink to="/company-list">Company List </CustomLink>
            </div>
 
-                <div onClick={()=>{profileButton()}} className='header-right'>
-                    <div className='profile-picture-header'></div>
-                    <div className='profile-name-header'>Sourav Das</div>
-                </div>  
+
+            <div className='header-right' >
+                <button className='Profile-button' onClick={()=>{profileButton()}} onBlur={()=>profileButtonBlur()} >
+                    <div>
+                        <div className='profile-picture-header'><img className='profile-picture-header' src={user.img}></img></div>
+                        <div className='profile-name-header'>{user.Name}</div>
+                    </div>
+                <div className={dropClass}>
+                    <a className="header-dropdown-menu" href="google.com">google</a>
+                    <a className="header-dropdown-menu" href="#">Link 2</a>
+                    <a className="header-dropdown-menu" href="#">Link 3</a>
+                </div>
+                </button>
+            
+</div> 
         </div>
     );
 };
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
 const Home = () => {
+    const [homeContent,setHomeContent]=useState({})
+    useEffect(()=>{
+        fetch
+    },[])
     return (
         <div>
             <h1>This is Home</h1>
+
+            {/* description  */}
+            
+            <div>
+                <div>
+                    <div className='Home-description-header'>
+                        <h1>
+
+                        </h1>
+                    </div>
+                    <div className='Home-description-text'>
+                        
+                    </div>
+                </div>
+                <div className='Home-description-image'>
+
+                </div>
+            </div>
         </div>
     );
 };
